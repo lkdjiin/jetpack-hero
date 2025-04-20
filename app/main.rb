1c840ab53dd0d6dc8947c4efe075a78fc5047db8
Xavier Nayrac <xavier.nayrac@gmail.com>
2025-04-20 17:37:55 +0200

    Use the jetpack

--- app/main.rb
+++ app/main.rb
@@ -1,25 +1,35 @@
-HERO_SCALE = 4
+HERO_SCALE = 4 # Image ratio
+FALL = -1.2 # Kind of gravity
+RL_SPEED = 5 # Right/left speed
+IMPULSE = 4 # Jetpack power
+IMPULSE_DECREASE = 0.9 # Jetpack power ratio decrease per frame
 
 def tick args
   args.state.hero ||= {
     x: 600,
-    y: 40,
+    y: 200,
     w: 7 * HERO_SCALE,
     h: 17 * HERO_SCALE,
-    path: 'sprites/hero.png'
+    path: 'sprites/hero.png',
+    impulse: 0,
   }
 
   if args.inputs.left
-    args.state.hero.x -= 5
+    args.state.hero.x -= RL_SPEED
   elsif args.inputs.right
-    args.state.hero.x += 5
+    args.state.hero.x += RL_SPEED
   end
 
-  if args.inputs.up
-    args.state.hero.y += 5
-  elsif args.inputs.down
-    args.state.hero.y -= 5
+  if args.inputs.keyboard.control || args.inputs.controller_one.y
+    args.state.hero.impulse = IMPULSE
   end
+
+  args.state.hero.impulse *= IMPULSE_DECREASE
+  args.state.hero.y += FALL
+  args.state.hero.y += args.state.hero.impulse
+
+  args.state.hero.x = args.state.hero.x.clamp(0, Grid.w - args.state.hero.w)
+  args.state.hero.y = args.state.hero.y.clamp(0, Grid.h - args.state.hero.h)
 
   args.outputs.solids << { x: 0, y: 0, w: 1280, h: 720, r: 0, g: 0, b: 0 }
   args.outputs.sprites << args.state.hero
