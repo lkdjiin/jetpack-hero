946a9b9071a86acb3dc4ae2b9c7bef4d5448bf39
Xavier Nayrac <xavier.nayrac@gmail.com>
2025-04-21 08:45:12 +0200

    Cleansing

--- app/main.rb
+++ app/main.rb
@@ -4,7 +4,14 @@ RL_SPEED = 5 # Right/left speed
 IMPULSE = 4 # Jetpack power
 IMPULSE_DECREASE = 0.9 # Jetpack power ratio decrease per frame
 
-def tick args
+def tick(args)
+  defaults(args)
+  render(args)
+  input(args)
+  calc(args)
+end
+
+def defaults(args)
   args.state.hero ||= {
     x: 600,
     y: 200,
@@ -13,7 +20,14 @@ def tick args
     path: 'sprites/hero.png',
     impulse: 0,
   }
+end
 
+def render(args)
+  args.outputs.solids << { x: 0, y: 0, w: 1280, h: 720, r: 0, g: 0, b: 0 }
+  args.outputs.sprites << args.state.hero
+end
+
+def input(args)
   if args.inputs.left
     args.state.hero.x -= RL_SPEED
   elsif args.inputs.right
@@ -23,14 +37,12 @@ def tick args
   if args.inputs.keyboard.control || args.inputs.controller_one.y
     args.state.hero.impulse = IMPULSE
   end
+end
 
+def calc(args)
   args.state.hero.impulse *= IMPULSE_DECREASE
   args.state.hero.y += FALL
   args.state.hero.y += args.state.hero.impulse
-
   args.state.hero.x = args.state.hero.x.clamp(0, Grid.w - args.state.hero.w)
   args.state.hero.y = args.state.hero.y.clamp(0, Grid.h - args.state.hero.h)
-
-  args.outputs.solids << { x: 0, y: 0, w: 1280, h: 720, r: 0, g: 0, b: 0 }
-  args.outputs.sprites << args.state.hero
 end
