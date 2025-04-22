ec94de2b31c02e09606472439299136739a91543
Xavier Nayrac <xavier.nayrac@gmail.com>
2025-04-22 16:27:02 +0200

    Fuel

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,5 @@
 HERO_SCALE = 4 # Image ratio
-FALL = -1.2 # Kind of gravity
+FALL = -1.8 # Kind of gravity
 RL_SPEED = 5 # Right/left speed
 IMPULSE = 4 # Jetpack power
 IMPULSE_DECREASE = 0.9 # Jetpack power ratio decrease per frame
@@ -18,24 +18,47 @@ def defaults(args)
     w: 7 * HERO_SCALE,
     h: 17 * HERO_SCALE,
     path: 'sprites/hero-flying.png',
+    flip_horizontally: false,
     impulse: 0,
     moving: :none,
-    flip_horizontally: false
+    jetpack_power: 100,
   }
 
-  args.state.platform ||= {
-    x: 400,
-    y: 400,
-    w: 300,
-    h: 12,
-    path: 'sprites/tile.png'
-  }
+  args.state.platforms ||= [
+    { x: 0, y: 570, w: 200, h: 12, path: 'sprites/tile.png' },
+    { x: 400, y: 570, w: 700, h: 12, path: 'sprites/tile.png' },
+    { x: 0, y: 420, w: 200, h: 12, path: 'sprites/tile.png' },
+    { x: 400, y: 420, w: 700, h: 12, path: 'sprites/tile.png' },
+    { x: 0, y: 270, w: 200, h: 12, path: 'sprites/tile.png' },
+    { x: 400, y: 270, w: 700, h: 12, path: 'sprites/tile.png' },
+    { x: 0, y: 130, w: 1280, h: 12, path: 'sprites/tile.png' },
+  ]
+
+  args.state.fuel ||= [
+    { x:700, y: 432, w: 25, h: 30, path: 'sprites/fuel.png', used: false },
+    { x:700, y: 582, w: 25, h: 30, path: 'sprites/fuel.png', used: false },
+  ]
 end
 
 def render(args)
-  args.outputs.solids << { x: 0, y: 0, w: 1280, h: 720, r: 0, g: 0, b: 0 }
-  args.outputs.sprites << args.state.platform
+  args.outputs.solids << { x: 0, y: 130, w: 1280, h: 610, r: 0, g: 0, b: 0 }
+  args.outputs.solids << { x: 0, y: 0, w: 1280, h: 130, r: 60, g: 60, b: 70 }
+  args.outputs.solids << { x: 305, y: 54, w: 600, h: 27, r: 255, g: 0, b: 0 }
+  args.outputs.solids << { x: 305, y: 54, w: args.state.hero.jetpack_power * 6, h: 27, r: 255, g: 255, b: 0 }
+  args.outputs.sprites << args.state.platforms
+  args.outputs.sprites << args.state.fuel
   args.outputs.sprites << args.state.hero
+  args.outputs.labels << {
+    x: 200,
+    y: 45,
+    size_px: 40,
+    alignment_enum: 0,
+    vertical_alignment_enum: 0,
+    text: "POWER",
+    r: 255,
+    g: 255,
+    b: 255,
+  }
 end
 
 def input(args)
@@ -48,7 +71,10 @@ def input(args)
   end
 
   if args.inputs.keyboard.control || args.inputs.controller_one.y
-    args.state.hero.impulse = IMPULSE
+    if args.state.hero.jetpack_power > 0
+      args.state.hero.impulse = IMPULSE
+      args.state.hero.jetpack_power -= 0.1
+    end
   end
 end
 
@@ -75,18 +101,27 @@ def calc(args)
     ascending = true
   end
 
-  if args.state.hero.intersect_rect?(args.state.platform)
-    if (x_before + args.state.hero.w) < args.state.platform.x
+  if p = Geometry.find_intersect_rect(args.state.hero, args.state.platforms)
+    if (x_before + args.state.hero.w) < p.x
       args.state.hero.x = x_before
-    elsif x_before >= (args.state.platform.x + args.state.platform.w)
+    elsif x_before >= (p.x + p.w)
       args.state.hero.x = x_before
     elsif ascending
-      args.state.hero.y = args.state.platform.y - args.state.hero.h - 2
+      args.state.hero.y = p.y - args.state.hero.h - 2
     else
       args.state.hero.path = 'sprites/hero-standing.png'
-      args.state.hero.y = args.state.platform.y + args.state.platform.h
+      args.state.hero.y = p.y + p.h
     end
   end
+
+  args.state.fuel.each do |f|
+    if args.state.hero.intersect_rect?(f)
+      args.state.hero.jetpack_power += 20
+      args.state.hero.jetpack_power = args.state.hero.jetpack_power.clamp(0, 100)
+      f.used = true
+    end
+  end
+  args.state.fuel.reject!(&:used)
 
   args.state.hero.x = args.state.hero.x.clamp(0, Grid.w - args.state.hero.w)
   args.state.hero.y = args.state.hero.y.clamp(0, Grid.h - args.state.hero.h)
